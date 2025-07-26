0c2b436815b54e0928e445c08791184c02ec1e7a
Oren Sayag <orensayag500@gmail.com>
2025-07-26 15:19:30 +0300

    :fire: removed redundant code

--- apps/app/src/app/app/achievements/page.tsx
+++ apps/app/src/app/app/achievements/page.tsx
@@ -1,7 +1,6 @@
-import { auth } from '@/auth';
 import { H1 } from '@/src/components/atoms/h1';
 
-export default async function AchievementsPage(){
-  // const session = await auth();
-  return <H1> Achievements page works!</H1>
+export default async function AchievementsPage() {
+    return <H1> Achievements page works!</H1>;
 }
+
