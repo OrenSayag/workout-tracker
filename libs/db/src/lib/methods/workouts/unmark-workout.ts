e7a0a73495b197c4ec14c0dc12da40dfd2b29ddf
NirKruter <nirkruter@gmail.com>
2025-07-29 22:03:28 +0300

    :sparkles: add removal of first workout logic

--- libs/db/src/lib/methods/workouts/unmark-workout.ts
+++ libs/db/src/lib/methods/workouts/unmark-workout.ts
@@ -1,19 +1,40 @@
 import { and, eq } from 'drizzle-orm';
 import { db } from '../../../config';
 import { workouts } from '../../schema';
+import { deleteUserAchievement } from '../achievements/delete-user-achievement';
+import { getUserWorkoutCount } from './get-user-workout-count';
+import { AchievementId } from '@workout-tracker/achievements';
 
 type Input = {
-    userId: string;
-    workoutDate: string;
+  userId: string;
+  workoutDate: string;
 };
 
+async function handleAchievements(input: Input) {
+  async function deleteFirstWorkout() {
+    const userWorkoutCount = await getUserWorkoutCount(input);
+    if (userWorkoutCount.workoutCount === 0) {
+      console.log('success?!');
+      const achievementData = {
+        userId: input.userId,
+        achievementId: AchievementId.FirstWorkout,
+      };
+
+      await deleteUserAchievement(achievementData);
+    }
+  }
+
+  await deleteFirstWorkout();
+}
+
 export const unmarkWorkout = async (input: Input) => {
-    await db
-        .delete(workouts)
-        .where(
-            and(
-                eq(workouts.userId, input.userId),
-                eq(workouts.workoutDate, input.workoutDate)
-            )
-        );
+  await db
+    .delete(workouts)
+    .where(
+      and(
+        eq(workouts.userId, input.userId),
+        eq(workouts.workoutDate, input.workoutDate)
+      )
+    );
+  await handleAchievements(input);
 };
