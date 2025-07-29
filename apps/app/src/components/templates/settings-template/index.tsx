fd22edd7179548b77605a44abdfc1e59986aec4b
NirKruter <nirkruter@gmail.com>
2025-07-29 23:41:16 +0300

    :construction: add the updateUserData component base

--- apps/app/src/components/templates/settings-template/index.tsx
+++ apps/app/src/components/templates/settings-template/index.tsx
@@ -1,10 +1,11 @@
 import { H1 } from '@/src/components/atoms/h1';
-
+import { UpdateUserData } from '@/src/components/update-user-data';
 
 export const AchievementsTemplate = () => {
   return (
     <div className="container mx-auto p-6 space-y-6">
       <H1>Settings</H1>
+      <UpdateUserData />
     </div>
   );
 };
