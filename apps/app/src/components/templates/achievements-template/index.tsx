3aabb83fd1b1301f1b87f77c1202f1512d254b6c
Oren Sayag <orensayag500@gmail.com>
2025-07-30 11:42:29 +0300

    :speech_balloon: achievements page - renamed subtitle for available achievement

--- apps/app/src/components/templates/achievements-template/index.tsx
+++ apps/app/src/components/templates/achievements-template/index.tsx
@@ -1,14 +1,12 @@
 import { H1 } from '@/src/components/atoms/h1';
-import {achievements } from '@workout-tracker/achievements';
 import { AchievementsList } from '@/src/components/templates/achievements-template/available-achievements';
 
-
 export const AchievementsTemplate = () => {
-  return (
-    <div className="container mx-auto p-6 space-y-6">
-      <H1>Achievements</H1>
-      <p className="text-muted-foreground">Possible Achievements</p>
-      <AchievementsList />
-    </div>
-  );
+    return (
+        <div className="container mx-auto p-6 space-y-6">
+            <H1>Achievements</H1>
+            <p className="text-muted-foreground">Available Achievements</p>
+            <AchievementsList />
+        </div>
+    );
 };
