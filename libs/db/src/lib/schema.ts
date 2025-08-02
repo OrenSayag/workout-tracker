1a148e5a268795960f52cc08794dba43fe68a39d
NirKruter <nirkruter@gmail.com>
2025-08-02 18:58:42 +0300

    :card_file_box: add id for user_achievements table

--- libs/db/src/lib/schema.ts
+++ libs/db/src/lib/schema.ts
@@ -8,87 +8,88 @@ import {
   primaryKey,
   timestamp,
   uuid,
-  varchar
+  varchar,
 } from 'drizzle-orm/pg-core';
 
 export const users = pgTable('users', {
-    id: varchar('id', { length: 255 }).primaryKey(),
-    createdAt: timestamp('created_at').defaultNow(),
-    updatedAt: timestamp('updated_at').$onUpdate(() => new Date()),
-    softDeletedAt: timestamp('soft_deleted_at'),
-    name: varchar({ length: 255 }).notNull(),
-    email: varchar('email', { length: 255 }).unique(),
-    emailVerified: timestamp('email_verified', {
-        mode: 'date',
-    }),
-    image: varchar('image', { length: 255 }),
-    phone: varchar('phone', { length: 20 }),
+  id: varchar('id', { length: 255 }).primaryKey(),
+  createdAt: timestamp('created_at').defaultNow(),
+  updatedAt: timestamp('updated_at').$onUpdate(() => new Date()),
+  softDeletedAt: timestamp('soft_deleted_at'),
+  name: varchar({ length: 255 }).notNull(),
+  email: varchar('email', { length: 255 }).unique(),
+  emailVerified: timestamp('email_verified', {
+    mode: 'date',
+  }),
+  image: varchar('image', { length: 255 }),
+  phone: varchar('phone', { length: 20 }),
 });
 
 export const betaUsers = pgTable('beta_users', {
-    id: uuid().primaryKey().defaultRandom(),
-    createdAt: timestamp('created_at').defaultNow(),
-    updatedAt: timestamp('updated_at').$onUpdate(() => new Date()),
-    email: varchar('email', { length: 255 }).unique(),
+  id: uuid().primaryKey().defaultRandom(),
+  createdAt: timestamp('created_at').defaultNow(),
+  updatedAt: timestamp('updated_at').$onUpdate(() => new Date()),
+  email: varchar('email', { length: 255 }).unique(),
 });
 
 export const sessions = pgTable('session', {
-    sessionToken: varchar('session_token', { length: 255 }).primaryKey(),
-    userId: varchar('user_id', { length: 255 })
-        .notNull()
-        .references(() => users.id, { onDelete: 'cascade' }),
-    expires: timestamp('expires', { mode: 'date' }).notNull(),
+  sessionToken: varchar('session_token', { length: 255 }).primaryKey(),
+  userId: varchar('user_id', { length: 255 })
+    .notNull()
+    .references(() => users.id, { onDelete: 'cascade' }),
+  expires: timestamp('expires', { mode: 'date' }).notNull(),
 });
 
 export const accounts = pgTable(
-    'account',
-    {
-        userId: varchar('user_id', { length: 255 })
-            .notNull()
-            .references(() => users.id, { onDelete: 'cascade' }),
-        type: varchar('type', { length: 255 }).notNull(),
-        provider: varchar('provider', { length: 255 }).notNull(),
-        providerAccountId: varchar('provider_account_id', {
-            length: 255,
-        }).notNull(),
-        refresh_token: varchar('refresh_token', { length: 255 }),
-        access_token: varchar('access_token', { length: 255 }),
-        expires_at: integer('expires_at'),
-        token_type: varchar('token_type', { length: 255 }),
-        scope: varchar('scope', { length: 255 }),
-        id_token: varchar('id_token', { length: 2048 }),
-        session_state: varchar('session_state', { length: 255 }),
-    },
-    (account) => ({
-        compoundKey: primaryKey({
-            columns: [account.provider, account.providerAccountId],
-        }),
-    })
+  'account',
+  {
+    userId: varchar('user_id', { length: 255 })
+      .notNull()
+      .references(() => users.id, { onDelete: 'cascade' }),
+    type: varchar('type', { length: 255 }).notNull(),
+    provider: varchar('provider', { length: 255 }).notNull(),
+    providerAccountId: varchar('provider_account_id', {
+      length: 255,
+    }).notNull(),
+    refresh_token: varchar('refresh_token', { length: 255 }),
+    access_token: varchar('access_token', { length: 255 }),
+    expires_at: integer('expires_at'),
+    token_type: varchar('token_type', { length: 255 }),
+    scope: varchar('scope', { length: 255 }),
+    id_token: varchar('id_token', { length: 2048 }),
+    session_state: varchar('session_state', { length: 255 }),
+  },
+  (account) => ({
+    compoundKey: primaryKey({
+      columns: [account.provider, account.providerAccountId],
+    }),
+  })
 );
 
 export const achievementId = pgEnum('achievement_id', [
-    AchievementId.FirstWorkout,
-    AchievementId.WeeklyStreak,
-    AchievementId.MonthlyStreak,
-    AchievementId.WeekendWarrior,
+  AchievementId.FirstWorkout,
+  AchievementId.WeeklyStreak,
+  AchievementId.MonthlyStreak,
+  AchievementId.WeekendWarrior,
 ]);
 
 export const workouts = pgTable('workouts', {
-    id: uuid().primaryKey().defaultRandom(),
-    userId: varchar('user_id', { length: 255 })
-        .notNull()
-        .references(() => users.id, { onDelete: 'cascade' }),
-    workoutDate: date('workout_date').notNull(),
-    createdAt: timestamp('created_at').defaultNow(),
-    updatedAt: timestamp('updated_at').$onUpdate(() => new Date()),
+  id: uuid().primaryKey().defaultRandom(),
+  userId: varchar('user_id', { length: 255 })
+    .notNull()
+    .references(() => users.id, { onDelete: 'cascade' }),
+  workoutDate: date('workout_date').notNull(),
+  createdAt: timestamp('created_at').defaultNow(),
+  updatedAt: timestamp('updated_at').$onUpdate(() => new Date()),
 });
 
 export const userAchievements = pgTable('user_achievements', {
-    createdAt: timestamp('created_at').defaultNow(),
-    updatedAt: timestamp('updated_at').$onUpdate(() => new Date()),
-    achievementId: achievementId('achievement_id').notNull(),
-    userId: varchar('user_id', { length: 255 })
-        .notNull()
-        .references(() => users.id, { onDelete: 'cascade' }),
+  id: uuid().primaryKey().defaultRandom(),
+  createdAt: timestamp('created_at').defaultNow(),
+  updatedAt: timestamp('updated_at').$onUpdate(() => new Date()),
+  achievementId: achievementId('achievement_id').notNull(),
+  userId: varchar('user_id', { length: 255 })
+    .notNull()
+    .references(() => users.id, { onDelete: 'cascade' }),
   isRead: boolean('is_read').default(false),
 });
